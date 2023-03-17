0e464dd5993ce0a1a56a045d0aecd7f7a2394d42
jatinyadav <jatinyadav09876@gmail.com>
2023-03-17 09:49:42 +0530

    Personal Portfolio Website

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multipletext",{
+    strings:["Frontend Developer","Graphic Designer","Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backdelay: 1000,
+    loop:true
+})
